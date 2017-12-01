5b1a767692c9e09e01bf264fcde0651bb81d3dc1
Scott Tiedemann <scott.tieds@gmail.com>
2017-12-01 14:37:44 -0500

    day one puzzles

---
+++ src/com/tieds/aoc2017/Main.java
@@ -0,0 +1,19 @@
+package com.tieds.aoc2017;
+
+import com.tieds.aoc2017.puzzle.DayOne;
+
+/**
+ * 2017 Advent of code solutions
+ * http://adventofcode.com/2017/day/1
+ * @author tieds
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//call puzzles
+		DayOne.solve();
+	}
+
+}
